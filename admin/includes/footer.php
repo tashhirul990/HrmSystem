aaabaea41cfda54344b048298282e7b797e965fe
Md Tashhirul Islam <tashhirul990@gmail.com>
2022-04-18 14:45:19 +0530

    Adding Files

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>@<a href="#">Md Tashhirul Islam</a></b>
+    </div>
+    <strong>Copyright &copy; 2019 HRM, Attendance and Payroll System </strong>
+</footer>
